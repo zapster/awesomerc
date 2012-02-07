daa7468028bdb5e18814175638f419bea36dac73
Josef Eisl <zapster@zapster.cc>
2012-02-07 17:01:04 +0100

    added Alt+F4 keybinding

--- rc.lua
+++ rc.lua
@@ -314,6 +314,9 @@ globalkeys = awful.util.table.join(
 clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
     awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end),
+    -- BEGIN Zap specific
+    awful.key({ altkey,           }, "F4",     function (c) c:kill()                         end),
+    -- END Zap specific
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
     awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
