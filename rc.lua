1e08b1bd5e0fd9c8e2c22b4df6d517c18cfa6726
Josef Eisl <zapster@zapster.cc>
2014-04-15 10:29:45 +0200

    added work related screens

--- rc.lua
+++ rc.lua
@@ -54,7 +54,7 @@ status_screen = 1
 if screen.count() == 1 then
   -- single monitor stuff
   tags = {
-    names = { "main", "mail", "im", "chat", "music", "gimp", 7, 8, 9 },
+    names = { "main", "mail", "im", "chat", "music", "gimp", 7, 8, 9, "thg", "eclipse", "graal", "igv", "c1vis", "F12"},
   }
   -- Each screen has its own tag table.
   tags[1] = awful.tag(tags.names, 1, layouts[1])
@@ -72,7 +72,7 @@ else
   -- set "status" screen
   tags[status_screen] = awful.tag({ 1, "mail", "im", "chat", "music", 6, 7, 8, 9 }, status_screen, layouts[1])
   -- set "main" screen
-  tags[main_screen] = awful.tag({ "main", 2, 3, 4, 5, 6, 7, 8, "gimp" }, main_screen, layouts[1])
+  tags[main_screen] = awful.tag({ "main", 2, 3, 4, 5, 6, 7, 8, "gimp", "thg", "eclipse", "graal", "igv", "c1vis", "F12" }, main_screen, layouts[1])
   for s = 3, screen.count() do
       -- Each screen has its own tag table.
       tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
@@ -85,6 +85,12 @@ tags.mail = tags[status_screen][2]
 tags.im = tags[status_screen][3]
 tags.chat = tags[status_screen][4]
 tags.music = tags[status_screen][5]
+tags.thg = tags[main_screen][10]
+tags.eclipse = tags[main_screen][11]
+tags.graal = tags[main_screen][12]
+tags.igv = tags[main_screen][13]
+tags.c1vis = tags[main_screen][14]
+tags.F12 = tags[main_screen][15]
 -- }}}
 
 -- {{{ Menu
@@ -506,6 +512,61 @@ globalkeys = awful.util.table.join(globalkeys,
         function ()
             awful.tag.viewonly(tags.gimp);
             awful.screen.focus(tags.gimp.screen)
+        end),
+    -- work related
+    awful.key({ modkey }, "F7",
+        function ()
+            awful.tag.viewonly(tags.thg);
+            awful.screen.focus(tags.thg.screen)
+        end),
+    awful.key({ modkey }, "F8",
+        function ()
+            awful.tag.viewonly(tags.eclipse);
+            awful.screen.focus(tags.eclipse.screen)
+        end),
+    awful.key({ modkey }, "F9",
+        function ()
+            awful.tag.viewonly(tags.graal);
+            awful.screen.focus(tags.graal.screen)
+        end),
+    awful.key({ modkey }, "F10",
+        function ()
+            awful.tag.viewonly(tags.igv);
+            awful.screen.focus(tags.igv.screen)
+        end),
+    awful.key({ modkey }, "F11",
+        function ()
+            awful.tag.viewonly(tags.c1vis);
+            awful.screen.focus(tags.c1vis.screen)
+        end),
+    awful.key({ modkey }, "F12",
+        function ()
+            awful.tag.viewonly(tags.F12);
+            awful.screen.focus(tags.F12.screen)
+        end),
+    awful.key({ modkey, "Shift" }, "F7",
+        function ()
+            awful.client.movetotag(tags.thg);
+        end),
+    awful.key({ modkey, "Shift" }, "F8",
+        function ()
+            awful.client.movetotag(tags.eclipse);
+        end),
+    awful.key({ modkey, "Shift" }, "F9",
+        function ()
+            awful.client.movetotag(tags.graal);
+        end),
+    awful.key({ modkey, "Shift" }, "F10",
+        function ()
+            awful.client.movetotag(tags.igv);
+        end),
+    awful.key({ modkey, "Shift" }, "F11",
+        function ()
+            awful.client.movetotag(tags.c1vis);
+        end),
+    awful.key({ modkey, "Shift" }, "F12",
+        function ()
+            awful.client.movetotag(tags.F12);
         end),
     -- move clients to screen
     awful.key({ modkey, "Shift" }, "F1",
