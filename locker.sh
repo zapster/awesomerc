d3b8395d6a47f313b0f7ebe18e240775795d7dc1
Josef Eisl <josef.eisl@oracle.com>
2019-01-09 09:40:35 +0100

    Add image to lock screen

--- locker.sh
+++ locker.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
+set -u
+set -e
+
+source="${BASH_SOURCE[0]}"
+while [ -h "$source" ] ; do
+    prev_source="$source"
+    source="$(readlink "$source")";
+    if [[ "$source" != /* ]]; then
+        # if the link was relative, it was relative to where it came from
+        dir="$( cd -P "$( dirname "$prev_source" )" && pwd )"
+        source="$dir/$source"
+    fi
+done
+location="$( cd -P "$( dirname "$source" )" && pwd )"
+
 exec xautolock -detectsleep \
   -time 5 \
-  -locker "i3lock -d -c 000070" \
+  -locker "i3lock -d -c 000070 -i $location/lockscreen-background.png" \
   -notify 30 \
   -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'" &
